6bab02ba7c17861e7c40537491e13f406a50f79a
Daniel-17 <frimudaniel17@gmail.com>
2023-02-11 13:05:00 +0100

    products route

--- 01_starting_project/src/App.jsx
+++ 01_starting_project/src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from "./pages/Home"
+import Products from "./pages/Products"
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }])
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
+])
 
 const App = () => {
   return <RouterProvider router={router} />
