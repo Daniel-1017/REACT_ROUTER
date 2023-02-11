04bd6ab82bbb121929bc83e9019139cf246705ab
Daniel-17 <frimudaniel17@gmail.com>
2023-02-11 13:12:54 +0100

    navigating between pages with links

--- 01_starting_project/src/App.jsx
+++ 01_starting_project/src/App.jsx
@@ -1,26 +1,26 @@
 import React from "react"
 import {
   createBrowserRouter,
-  createRoutesFromElements,
+  // createRoutesFromElements,
   RouterProvider,
-  Route,
+  // Route,
 } from "react-router-dom"
 import Home from "./pages/Home"
 import Products from "./pages/Products"
 
-const routeDifinition = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<Home />} />
-    <Route path="/products" element={<Products />} />
-  </Route>
-)
+// const routeDifinition = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// )
 
-// const router = createBrowserRouter([
-//   { path: "/", element: <Home /> },
-//   { path: "/products", element: <Products /> },
-// ])
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
+])
 
-const router = createBrowserRouter(routeDifinition)
+// const router = createBrowserRouter(routeDifinition)
 
 const App = () => {
   return <RouterProvider router={router} />
--- 01_starting_project/src/pages/Home.jsx
+++ 01_starting_project/src/pages/Home.jsx
@@ -1,7 +1,15 @@
-import React from "react"
+import React, { Fragment } from "react"
+import { Link } from "react-router-dom"
 
 const Home = () => {
-  return <h1>My Home Page</h1>
+  return (
+    <Fragment>
+      <h1>My Home Page</h1>
+      <p>
+        Go to <Link to="/products">The list of products</Link>.
+      </p>
+    </Fragment>
+  )
 }
 
 export default Home
