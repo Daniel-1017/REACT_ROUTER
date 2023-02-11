22d19b5eb8b294f0059d223ffb58ad018fec2302
Daniel-17 <frimudaniel17@gmail.com>
2023-02-11 19:29:57 +0100

    extracting error data & throwing responses

--- 02_new_project/frontend/src/Pages/Error.jsx
+++ 02_new_project/frontend/src/Pages/Error.jsx
@@ -1,7 +1,31 @@
-import React from "react"
+import React, { Fragment } from "react"
+import { useRouteError } from "react-router-dom"
+import MainNavigation from "../components/MainNavigation"
+import PageContent from "../components/PageContent"
 
 const Error = () => {
-  return <h1>An Error Occurred</h1>
+  const error = useRouteError()
+
+  let title = "An error occured"
+  let message = "Something went wrong!"
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message
+  }
+
+  if (error.status === 404) {
+    title = "Not Found!"
+    message = "Could not find resource or page!"
+  }
+
+  return (
+    <Fragment>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </Fragment>
+  )
 }
 
 export default Error
