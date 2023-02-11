b42a0d0060c308037c04edd7d76d218537e4f024
Daniel-17 <frimudaniel17@gmail.com>
2023-02-11 13:08:32 +0100

    difinign routes with jsx code

--- 01_starting_project/src/App.jsx
+++ 01_starting_project/src/App.jsx
@@ -1,12 +1,26 @@
 import React from "react"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom"
 import Home from "./pages/Home"
 import Products from "./pages/Products"
 
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/products", element: <Products /> },
-])
+const routeDifinition = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Home />} />
+    <Route path="/products" element={<Products />} />
+  </Route>
+)
+
+// const router = createBrowserRouter([
+//   { path: "/", element: <Home /> },
+//   { path: "/products", element: <Products /> },
+// ])
+
+const router = createBrowserRouter(routeDifinition)
 
 const App = () => {
   return <RouterProvider router={router} />
