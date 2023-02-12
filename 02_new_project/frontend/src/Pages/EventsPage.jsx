5c8d872e6a41623c50fef5547932d57dfdbee971
Daniel-17 <frimudaniel17@gmail.com>
2023-02-12 11:03:56 +0100

    json() utility function

--- 02_new_project/frontend/src/Pages/EventsPage.jsx
+++ 02_new_project/frontend/src/Pages/EventsPage.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useLoaderData } from "react-router-dom"
+import { json, useLoaderData } from "react-router-dom"
 
 import EventsList from "../components/EventsList"
 
@@ -20,9 +20,10 @@ export async function loader() {
 
   if (!response.ok) {
     // return { isError: true, message: "Could not fetch events." }
-    throw new Response(JSON.stringify({ message: "Could not fetch events!" }), {
-      status: 500,
-    })
+    // throw new Response(JSON.stringify({ message: "Could not fetch events!" }), {
+    //   status: 500,
+    // })
+    throw json({ message: "Could not fetch events!" }, { status: 500 })
   } else {
     return response
   }
